725f834f259cf46ae659c6f8d938686c2633cc9f
Zul <76121296+ZulTheDev@users.noreply.github.com>
2024-12-29 02:09:07 +0000

    flag output

--- wgmy/2024/misc/The DCM Meta/script.py
+++ wgmy/2024/misc/The DCM Meta/script.py
@@ -12,7 +12,11 @@ dicom_byte = [
 
 dicom_array = [25, 10, 0, 3, 17, 19, 23, 27, 4, 13, 20, 8, 24, 21, 31, 15, 7, 29, 6, 1, 9, 30, 22, 5, 28, 18, 26, 11, 2, 14, 16, 12]
 
-for i in dicom_array:     
-    base = dicom_byte[i].decode('utf-8').split('\x00')     
-    flag = f"WGMY {''.join(base).strip()}"
-    print(flag, end=' ')
+# for i in dicom_array:     
+#     base = dicom_byte[i].decode('utf-8').split('\x00')     
+#     flag = f"WGMY {''.join(base).strip()}"
+#     print(flag, end=' ')
+
+# GPT help with the line ouput
+output = ''.join(''.join(dicom_byte[i].decode('utf-8').split('\x00')).strip() for i in dicom_array)
+print(f"WGMY{output}")
